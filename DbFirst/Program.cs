6f370b616922093fec14c2e29dcace30c0144475
Hamza Elsharkawy <$ git config --local user.name Hamza>
2021-12-25 20:42:48 +0200

    Add project files.

---
+++ DbFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindEntities context = new NorthwindEntities())
+            {
+                var name = context.Categories.Find(9);
+                context.Categories.RemoveRange(name);
+                context.SaveChanges();
+                Console.WriteLine("change were sucessfully made");
+            }
+        }
+    }
+}
